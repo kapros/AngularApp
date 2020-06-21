07ed738402ba3a50bbe210fef8da644fb608d8e5
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-06-21 21:33:39 +0200

    add concrete AuthRepository

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,74 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _ctx;
+
+        public AuthRepository(DataContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<User> LoginAsync(string username, string password)
+        {
+            var user = await _ctx.Users.FirstOrDefaultAsync(x => x.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
+            if (user == null)
+                return null;
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+            return user;
+        }
+
+        public async Task<User> RegisterAsync(User user, string password)
+        {
+            byte[] passwordHash;
+            byte[] passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _ctx.Users.AddAsync(user);
+            await _ctx.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task<bool> UserExistsAsync(string username)
+        {
+            if (await _ctx.Users.AnyAsync(x => x.Username == username))
+                return true;
+            return false;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[0])
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
