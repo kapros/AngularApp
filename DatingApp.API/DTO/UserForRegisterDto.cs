a6bcaba3838f230fedcece4e60817050b28ea0eb
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-06-21 22:13:32 +0200

    add validation to UserForRegisterDto

--- DatingApp.API/DTO/UserForRegisterDto.cs
+++ DatingApp.API/DTO/UserForRegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace DatingApp.API.DTO
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Password must be betwen 4 and 8 characters")]
         public string Password { get; set; }
     }
 }
