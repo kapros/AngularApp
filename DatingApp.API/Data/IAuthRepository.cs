0b2f26019606f7cd75fcf042d0eb62d00a49acec
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-06-21 21:15:49 +0200

    add IAuthRepostiroy interface

---
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,17 @@
+﻿using DatingApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> RegisterAsync(User user, string password);
+
+        Task<User> LoginAsync(string username, string password);
+
+        Task<bool> UserExistsAsync(string username);
+    }
+}
