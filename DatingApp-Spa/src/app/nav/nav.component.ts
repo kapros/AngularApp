919ecdaa2ea427721cea9ec955c09ac367eaf5ad
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-06-22 21:27:50 +0200

    use AuthService login

--- DatingApp-Spa/src/app/nav/nav.component.ts
+++ DatingApp-Spa/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,12 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model:any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   login(){
-    console.log(this.model);
+    this.authService.login(this.model)
+    .subscribe(next => {
+      console.log('logged in successful');
+    }, error => {
+      console.log('error');
+    });
   }
 }
