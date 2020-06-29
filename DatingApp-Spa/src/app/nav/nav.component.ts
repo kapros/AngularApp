2f76692870959af8788e8c9f46c4bb60a2cf9bad
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-06-29 18:29:57 +0200

    change error logging

--- DatingApp-Spa/src/app/nav/nav.component.ts
+++ DatingApp-Spa/src/app/nav/nav.component.ts
@@ -29,7 +29,7 @@ export class NavComponent implements OnInit {
     .subscribe(next => {
       console.log('logged in successful');
     }, error => {
-      console.log('error');
+      console.log(error);
     });
   }
 }
