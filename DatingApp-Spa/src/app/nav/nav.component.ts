64e3250bd8b5078f3993081e88e730919a9b1f8d
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-06-29 19:41:13 +0200

    add alertify to nav bar login / logout

--- DatingApp-Spa/src/app/nav/nav.component.ts
+++ DatingApp-Spa/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,7 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model:any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit(): void {
   }
@@ -21,15 +22,15 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
   login(){
     this.authService.login(this.model)
     .subscribe(next => {
-      console.log('logged in successful');
+      this.alertify.success('logged in successful');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 }
