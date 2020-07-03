0f21f9291cff4daf1a026dcc64e20fa4030859b7
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-07-03 21:56:41 +0200

    add implementation for IDatingRepository

---
+++ DatingApp.API/Data/DatingRepository.cs
@@ -0,0 +1,48 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class DatingRepository : IDatingRepository
+    {
+        public DatingRepository(DataContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public DataContext _ctx;
+
+        public void Add<T>(T entity) where T : class
+        {
+            _ctx.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _ctx.Remove(entity);
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            var user = await _ctx.Users.Include(x => x.Photos).FirstOrDefaultAsync(x => x.Id == id);
+
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users = await _ctx.Users.Include(x => x.Photos).ToListAsync();
+
+            return users;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            return await _ctx.SaveChangesAsync() > 0;
+        }
+    }
+}
