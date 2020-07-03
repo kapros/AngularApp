aa676428cce3f757ec958592291fd38733e2d966
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-07-03 21:51:01 +0200

    fix typo

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -10,7 +10,7 @@ namespace DatingApp.API.Data
     {
         void Add<T>(T entity) where T : class;
 
-        void Delete<T>(T Entity) where T : class;
+        void Delete<T>(T entity) where T : class;
 
         Task<bool> SaveAll();
         
