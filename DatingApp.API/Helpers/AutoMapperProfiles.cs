35a2b37c353d01da70c1fb3910a35ebd1223db6d
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-07-03 23:03:58 +0200

    add automapper profiles

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using DatingApp.API.DTO;
+using DatingApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDTO>()
+                .ForMember(
+                    user => user.PhotoUrl, 
+                    opt => opt.MapFrom(x => x.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(
+                    user => user.Age, 
+                    opt => opt.MapFrom(x => x.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailedDTO>()
+                .ForMember(
+                    user => user.PhotoUrl, 
+                    opt => opt.MapFrom(x => x.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(
+                    user => user.Age, 
+                    opt => opt.MapFrom(x => x.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotosForDetailedDTO>();
+
+        }
+    }
+}
