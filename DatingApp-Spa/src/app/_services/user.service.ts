6ddc77f2d401fb0ea391eea2cf0c00b82d885e7a
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-07-04 13:35:12 +0200

    add methods to User service

--- DatingApp-Spa/src/app/_services/user.service.ts
+++ DatingApp-Spa/src/app/_services/user.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': 'Bearer ' + localStorage.getItem('token')
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +17,13 @@ export class UserService {
 
   baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers() : Observable<User[]> {
+    return this.httpClient.get<User[]>(this.baseUrl + "users", httpOptions);
+  }
+
+  getUser(id) : Observable<User> {
+    return this.httpClient.get<User>(this.baseUrl + "users/" + id, httpOptions);
+  }
 }
