695f716732b6f55762f46fb147d6a82132825ba6
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-07-04 14:47:20 +0200

    add loading user on init

--- DatingApp-Spa/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-Spa/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadUser();
   }
 
+  loadUser() {
+    this.userService.getUser(+this.route.snapshot.params['id]']).subscribe((user :User) => {
+      this.user = user;
+    }, error => {
+      this.alertify.error(error);
+    })
+  }
 }
