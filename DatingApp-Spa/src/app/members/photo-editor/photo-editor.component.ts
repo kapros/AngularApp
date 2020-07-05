532c367097efef37980def3769038df0a08bad53
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-07-05 17:29:19 +0200

    add first part of photo upload

---
+++ DatingApp-Spa/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { Photo } from 'src/app/_models/photo';
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/_services/auth.service';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css']
+})
+export class PhotoEditorComponent implements OnInit {
+  @Input() photos: Photo[];
+
+  uploader: FileUploader;
+  hasBaseDropZoneOver: boolean;
+  response: string;
+  baseUrl = environment.apiUrl;
+ 
+  constructor (private auth: AuthService){
+    this.hasBaseDropZoneOver = false;
+ 
+    this.response = '';
+ 
+    this.uploader.response.subscribe( res => this.response = res );
+  }
+
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.auth.decodedToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['images'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+      });
+  }
+}
