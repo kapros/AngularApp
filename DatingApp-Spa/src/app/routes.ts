7fce9a270a2324ef05163cac1acfb9770de33531
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-07-05 11:12:00 +0200

    add edit profile component

--- DatingApp-Spa/src/app/routes.ts
+++ DatingApp-Spa/src/app/routes.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -17,6 +18,7 @@ export const appRoutes: Routes = [
         children: [
             { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver} },
             { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver} },
+            { path: 'members/edit', component: MemberEditComponent}
             { path: 'messages', component: MessagesComponent },
             { path: 'lists', component: ListsComponent }
         ] 
