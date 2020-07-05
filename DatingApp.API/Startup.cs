65609a3b19bcdde31e91299b8db0d7086e3576db
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-07-05 15:35:32 +0200

    add cloudinary

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -41,7 +41,9 @@ namespace DatingApp.API
                     opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CLoudinarySettings"));
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
