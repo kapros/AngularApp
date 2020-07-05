36acb11987753b849839fbbb1be6cbb91aa7be8c
Kamil Prosowski <kamil.prosowski@rydoo.com>
2020-07-05 12:13:20 +0200

    finish edit page with navigation guard and saving client side

--- DatingApp-Spa/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-Spa/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm', {static: true}) editForm: NgForm;
+  user: User;
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+  
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile updated successfully!');
+    this.editForm.reset(this.user);
   }
 
 }
